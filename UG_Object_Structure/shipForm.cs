6758a0699888e21fda1d08a06dedc3d50ff96a8f
Russell Ericksen <russbossmoon@gmail.com>
2014-02-27 13:57:07 -0700

    added base project files

---
+++ UG_Object_Structure/shipForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UG_Object_Structure
+{
+    public partial class shipForm : Form
+    {
+        public shipForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
