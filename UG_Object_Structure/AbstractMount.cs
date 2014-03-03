5dae1b540a5a015e9abb4d8d438b893bd5f9b484
Russell Ericksen <russbossmoon@gmail.com>
2014-03-02 20:40:34 -0700

    added MobileObject.cs added dockMount.cs added abstractMount.cs

---
+++ UG_Object_Structure/AbstractMount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UG_Object_Structure
+{
+    abstract class AbstractMount
+    {
+        //link and unlink methods
+        //rotation data
+
+    }
+}
---
+++ UG_Object_Structure/MobileObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UG_Object_Structure
+{
+    abstract class MobileObject : AbstractObject
+    {
+        int cargoSpace = 0;
+        List<dockMount> dockMount;
+        List<int> weaponMount;
+        List<int> sensorMount;
+        List<int> shieldMount;
+        List<int> externalMount; 
+        List<int> equipment;
+
+        //methods for retreiving mounts
+        //methods for adding/removing a reference from a mount
+
+    }
+}
