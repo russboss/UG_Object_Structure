e65b124b8c24e757f0d6bf89a0cf870189343a53
Russell Ericksen <russbossmoon@gmail.com>
2014-03-05 12:00:44 -0700

    added OrbitalObject.cs

---
+++ UG_Object_Structure/OrbitalObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UG_Object_Structure
+{
+    class OrbitalObject : AbstractObject
+    {
+        double gravity = 9.8;
+        double spinSpeed = 1; //research into what units this should be
+
+        OrbitalObject parent = null;
+        List<OrbitalObject> child;
+
+        //List<ResourceType> resource;
+
+    }
+}
