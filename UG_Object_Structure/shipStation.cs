5c14beff4717dce51ad713b8a6a0146a2765e8c0
Russell Ericksen <russbossmoon@gmail.com>
2014-03-13 14:36:03 -0600

    added shipStation.cs

---
+++ UG_Object_Structure/shipStation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UG_Object_Structure
+{
+    class shipStation : MobileObject
+    {
+        //List<shipEngine> engine = null;
+        //methods to add and remove engines
+        //cruseDrive cruzeDrive = null;
+        //jumpDrive jumpDrive
+        Boolean stationMode = false;
+
+
+
+        public shipStation(int baseMass)//, shipEngine> engine)
+        {
+            setMass(baseMass);
+            //engine = new List<shipEngine>
+
+        }
+    }
+}
